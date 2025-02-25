b768928fa60029e6538a7ee1117bdb492fbbcdee
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-02-25 15:42:05 -0300

    fix: adding ngstyle for colors

--- src/app/components/weapon-case/weapon-case.component.ts
+++ src/app/components/weapon-case/weapon-case.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 
@@ -9,7 +10,7 @@ export interface WeaponCase {
 }
 
 const weapon_case: WeaponCase[] = [
-  {raridade: 'Grau de especificação militar	', cor: 'Azul escuro', chances: 79.92, probabilidade: '4 em 5'},
+  {raridade: 'Grau de especificação militar', cor: 'Azul escuro', chances: 79.92, probabilidade: '4 em 5'},
   {raridade: 'Restrito', cor: 'Roxo', chances: 15.98, probabilidade: '1 em 6'},
   {raridade: 'Classificado', cor: 'Rosa', chances: 3.2, probabilidade: '1 em 31'},
   {raridade: 'Encoberto', cor: 'Vermelho', chances: 0.64, probabilidade: '1 em 156'},
@@ -19,13 +20,22 @@ const weapon_case: WeaponCase[] = [
 @Component({
   selector: 'app-weapon-case',
   imports: [
-    MatTableModule
+    MatTableModule,
+    CommonModule
   ],
   templateUrl: './weapon-case.component.html',
   styleUrl: './weapon-case.component.css'
 })
 
 export class WeaponCaseComponent {
+   colorMap: { [key: string]: string } = {
+    'Grau de especificação militar': '#4169E1', // Azul escuro
+    'Restrito': '#800080', // Roxo
+    'Classificado': '#FF1493', // Rosa
+    'Encoberto': '#FF0000', // Vermelho
+    'Extremamente Raro': '#FFD700' // Ouro
+  };
+  
   displayedColumns: string[] = ['raridade', 'cor', 'chances', 'probabilidade'];
   dataSource = weapon_case;
 
