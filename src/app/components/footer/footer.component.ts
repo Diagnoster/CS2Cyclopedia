2ee745c97c456ad9b879f97f0b53f35303822caf
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-02-25 15:17:56 -0300

    feat: using component in footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,24 +1,43 @@
 import { Component } from '@angular/core';
-import { Cs2HelperService } from '../../services/cs2-helper.service';
 import { WeaponCaseComponent } from '../weapon-case/weapon-case.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-footer',
   imports: [
-    MatDialogModule
+    MatDialogModule,
+    CommonModule,
+    MatTableModule,
+    WeaponCaseComponent
   ],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
+  animations: [
+    trigger('slideUp', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate('300ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ transform: 'translateY(100%)', opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class FooterComponent {
+  isTableVisible = false;
 
   constructor(private dialog: MatDialog) {}
 
-  openModal(): void {
-    this.dialog.open(WeaponCaseComponent, {
-      width: '500px',
-    });
+  showTable() {
+    this.isTableVisible = true;
+  }
+
+  hideTable() {
+    this.isTableVisible = false;
   }
 
 }
