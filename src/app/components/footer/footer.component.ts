48feb9b35f79ade8eca796fdb39df01c4fae608f
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-02-25 17:01:02 -0300

    feat: removing odds from footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,43 +1,17 @@
 import { Component } from '@angular/core';
-import { WeaponCaseComponent } from '../weapon-case/weapon-case.component';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { animate, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-footer',
   imports: [
-    MatDialogModule,
-    CommonModule,
-    MatTableModule,
-    WeaponCaseComponent
+    CommonModule
   ],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css',
-  animations: [
-    trigger('slideUp', [
-      transition(':enter', [
-        style({ transform: 'translateY(70%)', opacity: 0 }),
-        animate('300ms ease-out', style({ transform: 'translateY(0)', opacity: 1 }))
-      ]),
-      transition(':leave', [
-        animate('300ms ease-in', style({ transform: 'translateY(100%)', opacity: 0 }))
-      ])
-    ])
-  ]
+  styleUrl: './footer.component.css'
 })
 export class FooterComponent {
   isTableVisible = false;
 
-  constructor(private dialog: MatDialog) {}
-
-  showTable() {
-    this.isTableVisible = true;
-  }
-
-  hideTable() {
-    this.isTableVisible = false;
-  }
+  constructor() {}
 
 }
