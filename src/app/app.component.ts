2bdd6acb028297841763ed93228cea88e1a69e73
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-02-25 11:40:56 -0300

    feat: creating footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CaseListComponent } from './components/case-list/case-list.component';
 import { CaseDetailsComponent } from './components/case-details/case-details.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
