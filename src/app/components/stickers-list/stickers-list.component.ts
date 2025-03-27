b3f46c100078f0b7a44f443dc2f98c4b6b35d5d0
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-03-27 13:06:18 -0300

    feat: adding lazy loading

--- src/app/components/stickers-list/stickers-list.component.ts
+++ src/app/components/stickers-list/stickers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Cs2ApiService } from '../../services/cs2-api.service';
 import { Cs2HelperService } from '../../services/cs2-helper.service';
 import { Sticker } from '../../models/sticker';
@@ -26,14 +26,31 @@ import { trigger, transition, style, animate } from '@angular/animations';
 export class StickersListComponent implements OnInit {
   stickers: Sticker[] = [];
   allStickers: Sticker[] = [];
+  loadedCount: number = 50;
 
-  constructor(private cs2Service: Cs2ApiService, private cs2Helper: Cs2HelperService) { }
+  constructor(private cs2Service: Cs2ApiService, private cs2Helper: Cs2HelperService) {}
 
   ngOnInit(): void {
     this.cs2Helper.changeCaseName('Stickers');
     this.cs2Service.getAllStickers().subscribe((data: any) => {
       this.allStickers = data;
-      this.stickers = [...this.allStickers];
+      this.loadMoreStickers(); // loading 50 stickers
     });
   }
+
+  loadMoreStickers(): void {
+    const nextBatch = this.allStickers.slice(this.stickers.length, this.stickers.length + this.loadedCount);
+    this.stickers = [...this.stickers, ...nextBatch];
+  }
+
+  onScroll(): void {
+    const scrollPosition = window.pageYOffset + window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+    const scrollPercentage = (scrollPosition / documentHeight) * 100;
+  
+    if (scrollPercentage > 90) {
+      this.loadMoreStickers();
+    }
+  }
+
 }
