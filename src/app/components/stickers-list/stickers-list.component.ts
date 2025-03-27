d9a90b506bd52b622226df1a950cd0e5a5c4ca20
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-03-27 11:26:52 -0300

    feat: adding stickers-list component

---
+++ src/app/components/stickers-list/stickers-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stickers-list',
+  imports: [],
+  templateUrl: './stickers-list.component.html',
+  styleUrl: './stickers-list.component.css'
+})
+export class StickersListComponent {
+
+}
