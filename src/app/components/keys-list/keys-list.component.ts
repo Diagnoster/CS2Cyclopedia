0b2bdcd97ac2b22503cb9bfd352b9ae10df3942d
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-03-28 15:54:25 -0300

    fix: adjustment key list style

--- src/app/components/keys-list/keys-list.component.ts
+++ src/app/components/keys-list/keys-list.component.ts
@@ -2,12 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { Cs2ApiService } from '../../services/cs2-api.service';
 import { Cs2HelperService } from '../../services/cs2-helper.service';
 import { Key } from '../../models/key';
+import { BaseFilterComponent } from '../base-filter/base-filter.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { NewlineToBrPipe } from "../../pipes/newline-to-br.pipe";
 
 @Component({
   selector: 'app-keys-list',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    BaseFilterComponent,
+    NewlineToBrPipe
+],
   templateUrl: './keys-list.component.html',
-  styleUrl: './keys-list.component.css'
+  styleUrl: './keys-list.component.css',
+  animations: [
+    trigger('fadeInDownBig', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-100px)' }),
+        animate('800ms cubic-bezier(0.23, 1, 0.32, 1)', style({ opacity: 1, transform: 'translateY(0)' }))
+      ])
+    ])
+  ]
 })
 export class KeysListComponent implements OnInit {
   keys: Key[] = [];
