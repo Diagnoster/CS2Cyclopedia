48260d14f318eda71bf11f97122adc6fa1dfdc83
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-03-28 15:34:01 -0300

    feat: adding keys component

---
+++ src/app/components/keys-list/keys-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-keys-list',
+  imports: [],
+  templateUrl: './keys-list.component.html',
+  styleUrl: './keys-list.component.css'
+})
+export class KeysListComponent {
+
+}
