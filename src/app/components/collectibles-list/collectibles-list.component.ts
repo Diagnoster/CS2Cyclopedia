692b067d8d2c1395236e41314ea4d206eba34309
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-04-02 13:33:04 -0300

    feat: adding html/css/animations

--- src/app/components/collectibles-list/collectibles-list.component.ts
+++ src/app/components/collectibles-list/collectibles-list.component.ts
@@ -2,12 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { Cs2ApiService } from '../../services/cs2-api.service';
 import { Cs2HelperService } from '../../services/cs2-helper.service';
 import { Collectible } from '../../models/collectible';
+import { NewlineToBrPipe } from "../../pipes/newline-to-br.pipe";
+import { BaseFilterComponent } from '../base-filter/base-filter.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-collectibles-list',
-  imports: [],
+  imports: [
+    NewlineToBrPipe,
+    BaseFilterComponent,
+    MatDividerModule
+  ],
   templateUrl: './collectibles-list.component.html',
-  styleUrl: './collectibles-list.component.css'
+  styleUrl: './collectibles-list.component.css',
+  animations: [
+    trigger('fadeInDownBig', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-100px)' }),
+        animate('800ms cubic-bezier(0.23, 1, 0.32, 1)', style({ opacity: 1, transform: 'translateY(0)' }))
+      ])
+    ])
+  ]
 })
 export class CollectiblesListComponent implements OnInit {
 
