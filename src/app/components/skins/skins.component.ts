df12708eb06ad9acd1f0e66861a269348e9a58fc
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-08-05 10:52:24 -0300

    feat: adding filter

--- src/app/components/skins/skins.component.ts
+++ src/app/components/skins/skins.component.ts
@@ -2,17 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Cs2ApiService } from '../../services/cs2-api.service';
 import { Skin } from '../../models/skin';
 import { MatDividerModule } from '@angular/material/divider';
+import { BaseFilterComponent } from '../base-filter/base-filter.component';
 
 @Component({
   selector: 'app-skins',
   imports: [
-    MatDividerModule
+    MatDividerModule,
+    BaseFilterComponent
   ],
   templateUrl: './skins.component.html',
   styleUrl: './skins.component.css'
 })
 export class SkinsComponent implements OnInit {
   skins: Skin [] = [];
+  allSkins: Skin [] = [];
 
   constructor(private cs2ApiService: Cs2ApiService) {}
 
@@ -22,7 +25,8 @@ export class SkinsComponent implements OnInit {
 
   getAllSkins() {
     this.cs2ApiService.getAllSkins().subscribe((data: any) => {
-      this.skins = data;
+      this.allSkins = data;
+      this.skins = [...this.allSkins];
       console.log(this.skins);
     });
   }
