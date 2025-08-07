6301f7bea2f52898d6ff5c39537f04812b90b90a
James Rovel Barbosa <67591330+Diagnoster@users.noreply.github.com>
2025-08-07 15:36:25 -0300

    feat: adding skin-details component

---
+++ src/app/components/skin-details/skin-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skin-details',
+  imports: [],
+  templateUrl: './skin-details.component.html',
+  styleUrl: './skin-details.component.css'
+})
+export class SkinDetailsComponent {
+
+}
